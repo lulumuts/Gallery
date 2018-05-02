c83fc0e9374c40e00994fe3f01450cf6f3866bb0
Lulu <lulumutuli24@gmail.com>
2018-05-02 17:46:12 +0300

    models.py updated with tables

--- gallery/photo/models.py
+++ gallery/photo/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'photos/',blank=True)
+    image_name = models.CharField(max_length=100)
+    image_description = models.TextField()
+
+    def __str__(self):
+        return self.image
+
+
+class Location(models.Model):
+    place = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.place
+
+class Category(models.Model):
+    name= models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
