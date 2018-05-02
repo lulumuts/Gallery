b44be41f3d088b7d11640db086db921b35f6aea1
Lulu <lulumutuli24@gmail.com>
2018-05-02 16:52:56 +0300

    base.html template displaying with navbar

--- gallery/photo/urls.py
+++ gallery/photo/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
+    url('^all/$',views.all_photos,name='photos')
 ]
--- gallery/photo/views.py
+++ gallery/photo/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime as dt
 
 # Create your views here.
 def welcome(request):
     return render(request,'main.html')
+
+def all_photos(request):
+    date = dt.date.today()
+    return render(request, 'all-photos/details.html', {"date" : date, })
