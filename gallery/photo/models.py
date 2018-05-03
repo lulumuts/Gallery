8bcaec30f7e5c46d307d67ca02b9fe32f84c2ce0
Lulu <lulumutuli24@gmail.com>
2018-05-03 12:16:14 +0300

    update image test passed

--- gallery/photo/models.py
+++ gallery/photo/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Image(models.Model):
-    image = models.ImageField(upload_to = 'photos/',blank=True)
-    image_name = models.CharField(max_length=100)
-    image_description = models.TextField()
-
-    def __str__(self):
-        return self.image
 
 
 class Location(models.Model):
@@ -16,8 +9,33 @@ class Location(models.Model):
     def __str__(self):
         return self.place
 
+
+    def save_location(self):
+        return self.save()
+
 class Category(models.Model):
     name= models.CharField(max_length=60)
 
     def __str__(self):
         return self.name
+
+    def save_name(self):
+        return self.save()
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'photos/',blank=True)
+    image_name = models.CharField(max_length=100)
+    image_description = models.TextField()
+    # image_location = models.ForeignKey(Location)
+    # image_category = models.ForeignKey(Category)
+
+    def __str__(self):
+        return self.image
+
+
+    def save_image(self):
+        return self.save()
+
+    @classmethod
+    def update_image(cls,id,image,image_name,image_description):
+        cls.objects.filter(id=id).update(image=image,image_name=image_name,image_description=image_description)
--- gallery/photo/tests.py
+++ gallery/photo/tests.py
@@ -1,3 +1,60 @@
 from django.test import TestCase
+from .models import Image, Location,Category
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+
+    def setUp(self):
+        self.image= Image(image_name='xmas', image_description = 'christmas in mallorca')
+
+    def tearDown(self):
+        Image.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image,Image))
+
+    def test_save_method(self):
+        self.image.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    def test_update_method(self):
+        self.image.save_image()
+        kwargs={'image':'/photo','image_name':'myname','image_description':'myself'}
+        self.image.update_image(self.image.id,**kwargs)
+        self.assertNotEqual("myname",self.image.image_name)
+
+
+
+
+class CategoryTestClass(TestCase):
+
+    def setUp(self):
+        self.name= Category(name= 'holiday')
+
+    def tearDown(self):
+        Category.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.name,Category))
+
+    def test_save_method(self):
+        self.name.save_name()
+        category = Category.objects.all()
+        self.assertTrue(len(category) > 0)
+
+class LocationTestClass(TestCase):
+
+    def setUp(self):
+        self.place= Location(place= 'mallorca')
+
+    def tearDown(self):
+        Location.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.place,Location))
+
+    def test_save_method(self):
+        self.place.save_location()
+        location = Location.objects.all()
+        self.assertTrue(len(location) > 0)
