884a9feb5bad91d7376df1224e758f9119c043dc
Lulu <lulumutuli24@gmail.com>
2018-05-05 21:36:31 +0300

    Update Django for whitenoise static

--- gallery/settings.py
+++ gallery/settings.py
@@ -128,8 +128,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,"static")]
-STATIC_ROOT =os.path.join(BASE_DIR,"staticfiles")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,"static"),
+)
+
+STATIC_ROOT =os.path.join(BASE_DIR,"live-static-files", "static-root")
+
 STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+MEDIA_ROOT = os.path.join(BASE_DIR,"live-static-files",'media-root')
