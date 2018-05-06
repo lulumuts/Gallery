a2d4964a8e5bc02a9c290ec6730068f17c7ff184
Lulu <lulumutuli24@gmail.com>
2018-05-06 08:15:37 +0300

    trying again with project_path in settings

--- gallery/settings.py
+++ gallery/settings.py
@@ -139,4 +139,4 @@ STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(PROJECT_PATH,'media')
