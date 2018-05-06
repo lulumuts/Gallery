ce185325da874438690220323c57605e841685cc
Lulu <lulumutuli24@gmail.com>
2018-05-06 08:56:45 +0300

    url.py updated with urlpatterns repath3

--- photo/urls.py
+++ photo/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles import views
+from django.urls import re_path
 
 urlpatterns=[
 
