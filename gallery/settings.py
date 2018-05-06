a99549699b68317dd63918a63f560197e099f0b3
Lulu <lulumutuli24@gmail.com>
2018-05-06 07:08:03 +0300

    white noise settings updated

--- gallery/settings.py
+++ gallery/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,7 +133,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR,"static"),
 )
 
-STATIC_ROOT =os.path.join(BASE_DIR,"live-static-files", "static-root")
+STATIC_ROOT =os.path.join(BASE_DIR,"staticfiles")
 
 STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
 
